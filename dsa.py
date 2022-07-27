a7706c3541a578d920e7fc61e286049dce8d1cd8
brianqp <brianqp9889@gmail.com>
2022-07-26 19:43:29 -0500

    inicializando

---
+++ dsa.py
@@ -0,0 +1,19 @@
+import binascii
+from random import random
+from Crypto.PublicKey import RSA
+from Crypto.Cipher import PKCS1_v1_5
+
+
+class Client:
+    def __init__(self):
+        self.private_key = RSA.generate(1024)
+        self.public_key = self.private_key.public_key()
+        self.signer = PKCS1_v1_5.new(self.public_key)
+
+    @property
+    def identity(self):
+        return binascii.hexlify(self.public_key.exportKey(format="DER")).decode('ascii')
+
+
+cliente = Client()
+print(cliente.identity)
